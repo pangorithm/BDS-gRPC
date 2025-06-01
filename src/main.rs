8768e8065acc03880ab26009be88ef850825939b
pangorithm <phs559800@gmail.com>
2025-06-01 12:37:56 +0900

    header jwt 인증 기능 추가

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,9 @@ use tonic::{Request, Response, Status, transport::Server};
 use hello::greeter_server::{Greeter, GreeterServer};
 use hello::{HelloReply, HelloRequest};
 
+mod interceptor;
+use interceptor::auth::AuthInterceptor;
+
 pub mod hello {
     tonic::include_proto!("hello"); // "hello"는 .proto의 package명
 }
@@ -26,6 +29,13 @@ struct Args {
         help = "port to listen on (default: 50051, must be writable by the user running this process)"
     )]
     port: String,
+
+    #[arg(
+        long,
+        default_value = "authorization",
+        help = "JWT token for authorization (default: authorization)"
+    )]
+    authorization: String,
 }
 
 #[derive(Debug, Default)]
@@ -52,12 +62,12 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let args = Args::parse();
 
     let addr = format!("{}:{}", args.host, args.port).parse()?;
+    let auth_interceptor = AuthInterceptor::new(args.authorization.clone());
     let greeter = MyGreeter::default();
-
     println!("Server listening on {}", addr);
 
     Server::builder()
-        .add_service(GreeterServer::new(greeter))
+        .add_service(GreeterServer::with_interceptor(greeter, auth_interceptor))
         .serve(addr)
         .await?;
 
