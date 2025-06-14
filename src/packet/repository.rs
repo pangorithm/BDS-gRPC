6536132cc830337cfe47d2da06acbf763bfdcce3
pangorithm <phs559800@gmail.com>
2025-06-14 15:41:32 +0900

    struct 구조체 파싱 오류로 인해 json string으로 변경

--- src/packet/repository.rs
+++ src/packet/repository.rs
@@ -1,7 +1,5 @@
+use serde_json::from_str;
 use tonic::Status;
-
-use crate::common::struct_to_value;
-use serde_json::Value as JsonValue;
 
 use crate::entity::packet_log::ActiveModel;
 use crate::entity::prelude::*;
@@ -20,10 +18,8 @@ impl PacketRepository {
     }
 
     pub async fn insert(&self, packet: Packet) -> Result<i32, Status> {
-        let json_value = packet
-            .des
-            .map(|s| struct_to_value(s))
-            .unwrap_or(JsonValue::Null);
+        let json_value = from_str(packet.des_json.as_str())
+            .map_err(|e| Status::invalid_argument(e.to_string()))?;
 
         let packet_log = ActiveModel {
             packet_id: NotSet,
